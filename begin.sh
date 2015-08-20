8eaab1f91ba97dbd9b5738350c762885e111482e
fataris <takihana.mail2@gmil.com>
2015-08-20 23:20:55 +0900

    0820_3 begin.sh bugfixed

--- begin.sh
+++ begin.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 #sudo pd-tone
 #sudo py-tone
-sudo pd-extended -nogui -alsa sound_ver9.pd &
+sudo pd-extended -nogui -alsa -audioindev 3 -audiooutdev 3 sound_ver9.pd &
 sudo python2 motion_button_ver4.py

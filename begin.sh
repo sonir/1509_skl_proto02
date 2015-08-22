eea35d15a610b9786f6bc3d69f3ff1bdab4d07a1
fataris <takihana.mail2@gmil.com>
2015-08-22 22:43:33 +0900

    0822 pyhton-revised and effect available

--- begin.sh
+++ begin.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 #sudo pd-tone
 #sudo py-tone
-sudo pd-extended -nogui -alsa -audioindev 3 -audiooutdev 3 sound_ver9.pd &
-sudo python2 motion_button_ver4.py
+sudo pd-extended -nogui -alsa -audioindev 3 -audiooutdev 3 sound_ver11.pd &
+sudo python2 motion_button_ver5.py
